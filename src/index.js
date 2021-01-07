e70ae64a95be77e4120602f5e97309f446aeac4e
Drew Thompson <drewallynthompson@gmail.com>
2021-01-07 10:40:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ function fetchToys(){
             'Content-Type':'application/json',
             'Accept':'application/json'
           }
-          body: JSON.stringify({'likes':parseInt()})
+          body: JSON.stringify({'likes':parseInt(toy.likes)})
         }
       }
       
