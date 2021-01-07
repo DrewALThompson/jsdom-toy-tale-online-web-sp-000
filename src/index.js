cac92d553b8df2832007214b1f8636d90ceac609
Drew Thompson <drewallynthompson@gmail.com>
2021-01-07 10:30:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ function fetchToys(){
       submit.addEventListener('submit', postData)
       
       function postData(){
-        let 
+        let input = 
         let data = 
       }
     }
